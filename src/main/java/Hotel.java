b3c4d37c882dc23d2fda22fac3ed9ad97d1e8bda
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-13 14:13:19 +0530

    add: special rates

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -2,12 +2,22 @@ public class Hotel implements Comparable<Hotel> {
     private final String name;
     private double regularWeekdayRates;
     private double regularWeekendRates;
+    private double specialWeekdayRates;
+    private double specialWeekendRates;
+
+    public enum CUSTOMER_TYPE {
+        REGULAR, REWARDS;
+    }
+
     private final int rating;
 
-    public Hotel(String name, double regularWeekdayRates, double regularWeekendRates, int rating) {
+    public Hotel(String name, double regularWeekdayRates, double regularWeekendRates, double specialWeekdayRates,
+            double specialWeekendRates, int rating) {
         this.name = name;
         this.regularWeekdayRates = regularWeekdayRates;
         this.regularWeekendRates = regularWeekendRates;
+        this.specialWeekdayRates = specialWeekdayRates;
+        this.specialWeekendRates = specialWeekendRates;
         this.rating = rating;
     }
 
@@ -35,16 +45,35 @@ public class Hotel implements Comparable<Hotel> {
         this.regularWeekdayRates = regularWeekdayRates;
     }
 
-    public double getPrice(String start, String end) {
+    public double getPrice(CUSTOMER_TYPE cType, String start, String end) {
         double rate;
-        rate = DateUtil.countSaturdaysAndSundays(start, end) * regularWeekendRates
-                + DateUtil.numberOfWeekdays(start, end) * regularWeekdayRates;
+        double weekendRates = cType.equals(CUSTOMER_TYPE.REGULAR) ? regularWeekendRates : specialWeekendRates;
+        double weekdayRates = cType.equals(CUSTOMER_TYPE.REGULAR) ? regularWeekdayRates : specialWeekdayRates;
+
+        rate = DateUtil.countSaturdaysAndSundays(start, end) * weekendRates
+                + DateUtil.numberOfWeekdays(start, end) * weekdayRates;
         return rate;
     }
 
     @Override
     public int compareTo(Hotel other) {
         return other.rating - this.rating;
+    }
+
+    public double getSpecialWeekdayRates() {
+        return specialWeekdayRates;
+    }
+
+    public void setSpecialWeekdayRates(double specialWeekdayRates) {
+        this.specialWeekdayRates = specialWeekdayRates;
+    }
+
+    public double getSpecialWeekendRates() {
+        return specialWeekendRates;
+    }
+
+    public void setSpecialWeekendRates(double specialWeekendRates) {
+        this.specialWeekendRates = specialWeekendRates;
     }
 
 }
--- src/main/java/HotelReservation.java
+++ src/main/java/HotelReservation.java
@@ -13,30 +13,32 @@ public class HotelReservation {
         return true;
     }
 
-    public static String getCheapestBestRatedHotel(String start, String end) {
+    public static String getCheapestBestRatedHotel(Hotel.CUSTOMER_TYPE cType, String start, String end) {
         Map<Hotel, Double> map = new TreeMap<>();
         for (Hotel hotel : hotelList) {
-            map.put(hotel, hotel.getPrice(start, end));
+            map.put(hotel, hotel.getPrice(cType, start, end));
         }
 
         Entry<Hotel, Double> result = map.entrySet().stream().sorted(Map.Entry.comparingByValue()).findFirst().get();
         return result.getKey().getName() + ", Rating: " + result.getKey().getRating() + ", Rate: " + result.getValue();
     }
 
-    public static String getBestRatedHotel(String start, String end) {
+    public static String getBestRatedHotel(Hotel.CUSTOMER_TYPE cType, String start, String end) {
         hotelList.sort(Comparator.naturalOrder());
-        return hotelList.get(0).getName() + ", Rate: " + hotelList.get(0).getPrice(start, end);
+        return hotelList.get(0).getName() + ", Rate: " + hotelList.get(0).getPrice(cType, start, end);
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to hotel reservation program");
-        addHotel(new Hotel("Lakewood", 110, 90, 3));
-        addHotel(new Hotel("Bridgewood", 150, 50, 4));
-        addHotel(new Hotel("Ridgewood", 220, 150, 5));
+        addHotel(new Hotel("Lakewood", 110, 90, 80, 80, 3));
+        addHotel(new Hotel("Bridgewood", 150, 50, 110, 50, 4));
+        addHotel(new Hotel("Ridgewood", 220, 150, 100, 40, 5));
         Scanner sc = new Scanner(System.in);
         String[] dates = sc.nextLine().replaceAll(" ", "").split(",");
         sc.close();
-        System.out.println(getBestRatedHotel(dates[0], dates[1]));
+        Hotel.CUSTOMER_TYPE cType = dates[0].equalsIgnoreCase("Regular") ? Hotel.CUSTOMER_TYPE.REGULAR
+                : Hotel.CUSTOMER_TYPE.REWARDS;
+        System.out.println(getBestRatedHotel(cType,dates[1], dates[2]));
 
     }
 }
