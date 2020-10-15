ef6dcd5c16cf3cf7e36c728b32547e3f30169aba
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-15 13:04:42 +0530

    refactor: unnecessary static declalarion removed

--- src/main/java/com/capgemini/training/java/hotelreservation/HotelReservation.java
+++ src/main/java/com/capgemini/training/java/hotelreservation/HotelReservation.java
@@ -4,9 +4,9 @@ import java.util.*;
 import java.util.Map.Entry;
 
 public class HotelReservation {
-    private static final List<Hotel> hotelList = new ArrayList<>();
+    private List<Hotel> hotelList = new ArrayList<>();
 
-    public static boolean addHotel(Hotel hotel) {
+    public boolean addHotel(Hotel hotel) {
         if (hotel == null)
             return false;
         if (hotelList.contains(hotel))
@@ -15,7 +15,7 @@ public class HotelReservation {
         return true;
     }
 
-    public static Entry<Hotel, Double> getCheapestBestRatedHotel(CUSTOMER_TYPE cType, String start, String end)
+    public Entry<Hotel, Double> getCheapestBestRatedHotel(CUSTOMER_TYPE cType, String start, String end)
             throws InvalidDateException {
         Map<Hotel, Double> map = new TreeMap<>();
         for (Hotel hotel : hotelList) {
@@ -26,7 +26,7 @@ public class HotelReservation {
 
     }
 
-    public static Entry<Hotel, Double> getBestRatedHotel(CUSTOMER_TYPE cType, String start, String end)
+    public Entry<Hotel, Double> getBestRatedHotel(CUSTOMER_TYPE cType, String start, String end)
             throws InvalidDateException {
         hotelList.sort(Comparator.naturalOrder());
         Hotel bestRatedHotel = hotelList.stream().sorted(Comparator.naturalOrder()).findFirst().get();
@@ -43,17 +43,18 @@ public class HotelReservation {
 
     public static void main(String[] args) throws InvalidHotelAttributeException, InvalidCustomerTypeException {
         System.out.println("Welcome to hotel reservation program");
-        addHotel(new Hotel("Lakewood", 110, 90, 80, 80, 3));
-        addHotel(new Hotel("Bridgewood", 150, 50, 110, 50, 4));
-        addHotel(new Hotel("Ridgewood", 220, 150, 100, 40, 5));
+        HotelReservation hotelReservation = new HotelReservation();
+        hotelReservation.addHotel(new Hotel("Lakewood", 110, 90, 80, 80, 3));
+        hotelReservation.addHotel(new Hotel("Bridgewood", 150, 50, 110, 50, 4));
+        hotelReservation.addHotel(new Hotel("Ridgewood", 220, 150, 100, 40, 5));
         Scanner sc = new Scanner(System.in);
         String[] dates = sc.nextLine().replaceAll(" ", "").split(",");
         sc.close();
-        if((!dates[0].equalsIgnoreCase("Regular"))&&(!dates[0].equalsIgnoreCase("Reward")))
+        if ((!dates[0].equalsIgnoreCase("Regular")) && (!dates[0].equalsIgnoreCase("Reward")))
             throw new InvalidCustomerTypeException();
         CUSTOMER_TYPE cType = dates[0].equalsIgnoreCase("Regular") ? CUSTOMER_TYPE.REGULAR : CUSTOMER_TYPE.REWARDS;
         try {
-            printToConsole(getCheapestBestRatedHotel(cType, dates[1], dates[2]));
+            printToConsole(hotelReservation.getCheapestBestRatedHotel(cType, dates[1], dates[2]));
         } catch (InvalidDateException e) {
             e.getMessage();
         }
--- src/test/java/com/capgemini/training/java/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/capgemini/training/java/hotelreservation/HotelReservationTest.java
@@ -8,12 +8,18 @@ import org.junit.Test;
 
 public class HotelReservationTest {
     public Hotel lakewood, bridgewood, ridgewood;
+    HotelReservation hotelReservation;
 
     @Before
     public void init() throws InvalidHotelAttributeException {
         lakewood = new Hotel("Lakewood", 110, 90, 80, 80, 3);
         bridgewood = new Hotel("Bridgewood", 150, 50, 110, 50, 4);
         ridgewood = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+
+        hotelReservation = new HotelReservation();
+        hotelReservation.addHotel(lakewood);
+        hotelReservation.addHotel(bridgewood);
+        hotelReservation.addHotel(ridgewood);
     }
 
     @Test
@@ -24,11 +30,8 @@ public class HotelReservationTest {
 
     @Test
     public void cheapestBestRatedForRewardCustomer() throws InvalidDateException {
-        HotelReservation.addHotel(lakewood);
-        HotelReservation.addHotel(bridgewood);
-        HotelReservation.addHotel(ridgewood);
 
-        Entry<Hotel, Double> result = HotelReservation.getCheapestBestRatedHotel(CUSTOMER_TYPE.REWARDS, "11Sep2020",
+        Entry<Hotel, Double> result = hotelReservation.getCheapestBestRatedHotel(CUSTOMER_TYPE.REWARDS, "11Sep2020",
                 "12Sep2020");
         Assert.assertEquals(ridgewood, result.getKey());
         Assert.assertEquals(Double.valueOf(140), result.getValue());
@@ -36,11 +39,8 @@ public class HotelReservationTest {
 
     @Test
     public void cheapestBestRatedForRegularCustomer() throws InvalidDateException {
-        HotelReservation.addHotel(lakewood);
-        HotelReservation.addHotel(bridgewood);
-        HotelReservation.addHotel(ridgewood);
 
-        Entry<Hotel, Double> result = HotelReservation.getCheapestBestRatedHotel(CUSTOMER_TYPE.REGULAR, "11Sep2020",
+        Entry<Hotel, Double> result = hotelReservation.getCheapestBestRatedHotel(CUSTOMER_TYPE.REGULAR, "11Sep2020",
                 "12Sep2020");
         Assert.assertEquals(bridgewood, result.getKey());
         Assert.assertEquals(Double.valueOf(200), result.getValue());
@@ -48,11 +48,8 @@ public class HotelReservationTest {
 
     @Test
     public void bestRatedForRegularCustomer() throws InvalidDateException {
-        HotelReservation.addHotel(lakewood);
-        HotelReservation.addHotel(bridgewood);
-        HotelReservation.addHotel(ridgewood);
 
-        Entry<Hotel, Double> result = HotelReservation.getBestRatedHotel(CUSTOMER_TYPE.REGULAR, "11Sep2020",
+        Entry<Hotel, Double> result = hotelReservation.getBestRatedHotel(CUSTOMER_TYPE.REGULAR, "11Sep2020",
                 "12Sep2020");
         Assert.assertEquals(ridgewood, result.getKey());
         Assert.assertEquals(Double.valueOf(370), result.getValue());
@@ -60,9 +57,7 @@ public class HotelReservationTest {
 
     @Test
     public void hotelAddInReservationSystem() {
-        Assert.assertTrue(HotelReservation.addHotel(lakewood));
-        Assert.assertTrue(HotelReservation.addHotel(bridgewood));
-        Assert.assertTrue(HotelReservation.addHotel(ridgewood));
-        Assert.assertFalse(HotelReservation.addHotel(lakewood)); // check duplicate
+
+        Assert.assertFalse(hotelReservation.addHotel(lakewood)); // check duplicate
     }
 }
